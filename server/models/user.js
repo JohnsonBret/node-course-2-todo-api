0e7c7b033431eb8b52fd30e97f7513f8f2d041ce
Bret Main <bretaudio@gmail.com>
2019-02-24 22:14:03 -0800

    add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -32,6 +32,7 @@ var UserSchema = new mongoose.Schema({
     }]
 });
 
+// UserSchema.methods adds and instance method
 UserSchema.methods.toJSON = function(){
     var user = this;
     var userObject = user.toObject();
@@ -49,6 +50,27 @@ UserSchema.methods.generateAuthToken = function(){
     return user.save().then(()=>{
         return token;
     });
+};
+
+//UserSchema.statics adds a model method
+UserSchema.statics.findByToken = function(token){
+    var User = this;
+    var decoded;
+
+    try{
+        decoded = jwt.verify(token, 'abc123');
+    } catch(e){
+
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+    
+
 };
 
 var User = mongoose.model('User', UserSchema);
