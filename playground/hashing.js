6a6efad484989611148efafacc954daeca915d3e
Bret Main <bretaudio@gmail.com>
2019-02-24 22:39:10 -0800

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,46 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+//================================
+// bcrypt
+//================================
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// var password = '123abc!';
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// Salt generation and password hash
 
+// bcrypt.genSalt(10, (err, salt)=>{
+//     bcrypt.hash(password, salt, (err, hash)=>{
+//         console.log(hash);
+//     })
+// });
+
+// Compare hashed and salted password with plain text
+
+// var hashedPassword = '$2a$10$/aS4tPjy7g2JIi66Eqgmm.Pt3qmTM7YM2AOtcflZ6RAqVT.F2xLT6';
+
+// bcrypt.compare(password, hashedPassword, (err, res)=>{
+//     console.log(res);
+// });
+
+//================================
+// Json Web Token - JWT
+//================================
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
+
+//================================
 //Hashing and Salt tutorial below
+//================================
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
